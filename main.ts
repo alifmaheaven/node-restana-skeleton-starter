6166a9f5149dbcb38cd9e694d4c6f61175d78bd7
alifmaheaven <mohammad.maheaven@sigma.co.id>
2024-03-07 11:32:12 +0700

    sweegers problem

--- main.ts
+++ main.ts
@@ -1,17 +1,23 @@
-require('dotenv').config()
+require('dotenv').config();
 
-const https = require('https')
-const restana = require('restana')
+const https = require('https');
+const restana = require('restana');
 
-const bodyParser = require('body-parser')
-const cors = require('cors')
+const bodyParser = require('body-parser');
+const path = require('path');
+const cors = require('cors');
 const helmet = require('helmet');
 
-const usersRouter = require('./routes/users')
+const {
+  SwaggerValidationError,
+  SwaggerValidator
+} = require('restana-swagger-validator')
+
+const usersRouter = require('./routes/users');
 
 // variabels
 var port = process.env.PORT || 3000;
-const isProduction = process.env.NODE_ENV === 'production'
+const isProduction = process.env.NODE_ENV === 'production';
 
 // config
 const service = restana({
@@ -19,21 +25,36 @@ const service = restana({
   //   // key: keys.serviceKey,
   //   // cert: keys.certificate
   // })
+  errorHandler: (err: any, req: any, res: any) => {
+    if (err instanceof SwaggerValidationError) {
+      res.statusCode = err.statusCode
+      res.send({
+        message: err.message,
+        errors: err.errors
+      })
+    } else {
+      res.send(err)
+    }
+  }
+});
+
+SwaggerValidator(service, path.join(__dirname, '/spec.json'), {
+  buildResponses: false
 })
 
 service.use(cors({
   origin: isProduction ? '*' : '*'
-}))
-service.use(helmet());
+}));
 
+// service.use(helmet());
 service.use(bodyParser.json({limit: '50mb'}));
 service.use(bodyParser.urlencoded({limit: '50mb', extended: true}));
 
-// routes
+// // routes
 
-service.use('/api/v1/users', usersRouter)
+service.use('/api/v1/users', usersRouter);
 
-service.get('/api', (req: any, res: any) => res.send('Api already create'));
+// service.get('/api', (req: any, res: any) => res.send('Api already create'));
 
 service.start(port).then(() => {
   console.log(`Server started on port :`+port+``);
--- routes/users.ts
+++ routes/users.ts
@@ -3,9 +3,12 @@
 require('dotenv').config()
 
 const userService = require('restana')();
-const nestedRouter = userService.newRouter()
+const path = require('path');
+
+const { SwaggerValidator } = require('restana-swagger-validator')
 
 // config
+const nestedRouter = userService.newRouter()
 
 // controllers
 const usersController = require('../controllers/usersController')
@@ -13,5 +16,13 @@ const usersController = require('../controllers/usersController')
 // routes
 nestedRouter.get('/', usersController.getData)
 nestedRouter.post('/login', usersController.login)
+
+SwaggerValidator(nestedRouter, path.join(__dirname, '/users-sweggers.json'), {
+  // buildResponses: true,
+  // requireSchemaSpec: true,
+  // apiSpecEndpoint: '/api/v1/users/swagger.json',
+  // uiEndpoint: '/docs',
+  publicApiEndpoint: '/api/v1/users'
+})
 
 module.exports = nestedRouter;
